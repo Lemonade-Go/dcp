3efbaf6e7814d7bc451cb610b15212107e95b1ac
venky kuberan <x3g5@nordstrom.com>
2018-01-21 20:41:02 -0800

    o(n) solution added

--- d2/go/d2-productexceptself.go
+++ d2/go/d2-productexceptself.go
@@ -6,7 +6,7 @@ func main() {
 
 	//Soultion with Hashmap, but i think its n^2 complexity
 	listinput := []int{1, 2, 3, 4, 5}
-	list1 := []int{}
+	// listinput := []int{1}
 	list2 := []int{}
 
 	hm := make(map[int]int)
@@ -27,53 +27,40 @@ func main() {
 
 	fmt.Println("List2 -- ", list2)
 
-	// o(n) solution - InProgress
-
+	// ====== o(n) solution ========
+	// Approach 2
 	list3 := [5]int{}
-	list4 := [5]int{}
+	// list4 := [5]int{}
 
-	p := 1
+	left := 1
 	for i := 0; i < len(listinput); i++ {
-		// list1 = append(list1, p)
-		list3[i] = p
-		p = p * listinput[i]
+		// list1 = append(list1, p) // append won't work here as it adds at end of the slice.
+		list3[i] = left
+		left = left * listinput[i]
 	}
 
-	p = 1
+	fmt.Println("Input List ", listinput)
+
+	right := 1
 	for i := len(listinput) - 1; i >= 0; i-- {
-		// list2 = append(list2, p) // append won't work here as it adds at end of the slice.
-		list4[i] = p
-		p = p * listinput[i]
+
+		// Approach 2.1 with 1 set of extra array
+		// list4[i] = right
+		// right = right * listinput[i]
+
+		temp := listinput[i]
+		listinput[i] = list3[i] * right
+		right = right * temp
+
 	}
+	// Approach 2.1 with 1 set of extra array
+	// for i := 0; i < len(listinput); i++ {
+	// 	listinput[i] = list3[i] * list4[i]
+	// 	fmt.Println("Values ====", list3[i], list4[i], listinput[i])
+	// }
 
-	fmt.Println(listinput)
-	for i := 0; i < len(list1); i++ {
-		listinput[i] = list3[i] * list4[i]
-	}
+	fmt.Println("Left array -- ", list3)
+	// fmt.Println("Right array --", list4)
+	fmt.Println("Output List ", listinput)
 
-	fmt.Println(list3)
-	fmt.Println(list4)
-	fmt.Println(listinput)
-
-	/*
-	   int a[N] // This is the input
-	   int products_below[N];
-	   p=1;
-	   for(int i=0;i<N;++i) {
-	     products_below[i]=p;
-	     p*=a[i];
-	   }
-
-	   int products_above[N];
-	   p=1;
-	   for(int i=N-1;i>=0;--i) {
-	     products_above[i]=p;
-	     p*=a[i];
-	   }
-
-	   int products[N]; // This is the result
-	   for(int i=0;i<N;++i) {
-	     products[i]=products_below[i]*products_above[i];
-	   }
-	*/
 }
